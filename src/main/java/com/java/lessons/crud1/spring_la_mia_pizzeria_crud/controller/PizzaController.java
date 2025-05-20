883b0edfea43c4e556838e4a8e45485f49d0dee5
alessiozaccato <alessiozaccato@gmail.com>
2025-05-20 13:05:42 +0200

    first commit

---
+++ src/main/java/com/java/lessons/crud1/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,5 @@
+package com.java.lessons.crud1.spring_la_mia_pizzeria_crud.controller;
+
+public class PizzaController {
+
+}
---
+++ src/main/java/com/java/lessons/crud1/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,74 @@
+package com.java.lessons.crud1.spring_la_mia_pizzeria_crud.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pizza")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @NotBlank(message = "")
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @NotBlank
+    private String description;
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @NotBlank
+    private String img_url;
+
+    public String getImgUrl() {
+        return this.img_url;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.img_url = imgUrl;
+    }
+    @NotNull
+    private BigDecimal price;
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    
+
+}
